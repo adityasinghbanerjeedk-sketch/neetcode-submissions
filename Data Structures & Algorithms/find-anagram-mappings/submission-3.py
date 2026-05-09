a329bdc522c1b723f5f9c2e33710ec3f274a659c
adityasinghbanerjeedk-sketch <adityasinghbanerjee.dk@gmail.com>
2026-05-09 18:15:12 +0530

    Add: find-anagram-mappings - submission-3

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap_1, hashmap_2 = {}, {}
+        lst = []
+        for idx, val in enumerate(nums2):
+            hashmap_2[val] = idx
+        for i in nums1:
+                # print(hashmap_2[i])
+                lst.append(hashmap_2[i])
+        
+        # print(lst)
+        return lst
+        # hashmap_1, hashmap_2 = defaultdict(list), defaultdict(list)
+        # for idx, val in enumerate(nums1):
+        #     hashmap_1[val].append(idx)
+        # for idx, val in enumerate(nums2):
+        #     hashmap_2[val].append(idx)
+
+        # print(hashmap_1)
